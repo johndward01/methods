3ebae24a0464a52fd7039758b9cdffd7bf3aa8fe
johndward01 <johndward01@gmail.com>
2023-05-15 15:41:17 -0500

    adding debug printing for structs

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+fn main() {
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+    //println!("rect1 is {:#?}", rect1);
+    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
